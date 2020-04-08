dc7fdd9adc015580171d68f0f22f81d07d86b9ab
DMdavydov <dmitry_davydov1@epam.com>
2020-04-08 10:10:04 +0300

    Initial commit

---
+++ src/test/java/com/ddavydov/bugtrackerusers/BugTrackerUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddavydov.bugtrackerusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
